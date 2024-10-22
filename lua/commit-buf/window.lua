cabbb4888a08a034370a03ef771b12fa0201c112
asana17 <kei17.kaight084@gmail.com>
2024-10-22 23:36:06 +0900

    feat(window.lua): use config when opening new window
    Signed-off-by: asana17 <kei17.kaight084@gmail.com>

--- lua/commit-buf/window.lua
+++ lua/commit-buf/window.lua
@@ -1,9 +1,33 @@
 local M = {}
 
+---@type table<string, any>
+local configs_default = {
+    split = "right",
+}
+
+---@alias win_handle integer
+---@type win_handle|nil could be nil, but should not be zero
+local handle_base
+
+---@type boolean
+local initialized = false
+
+---@return nil
+local function update_config()
+  configs_default["win"] = handle_base
+end
+
+---open window using config
 ---@return nil
 function M.open()
-  vim.cmd('vsplit')
-  vim.cmd('enew')
+  if not initialized then
+    initialized = true
+    handle_base = vim.api.nvim_get_current_win()
+  end
+
+  update_config()
+
+  vim.api.nvim_open_win(0, true, configs_default)
 end
 
 return M
