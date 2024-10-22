b724102d657c951597c835511d2973f493aeeeb6
asana17 <kei17.kaight084@gmail.com>
2024-10-23 01:29:52 +0900

    feat: utility function to get system command result
    Signed-off-by: asana17 <kei17.kaight084@gmail.com>

---
+++ lua/commit-buf/utils.lua
@@ -0,0 +1,37 @@
+local M = {}
+
+---@param result_table table<string>|nil
+---@return nil
+local function remove_whiteline(result_table)
+  while result_table and result_table[#result_table] == "" do
+    table.remove(result_table)
+  end
+end
+
+---run command synchronously and output result string table
+---pass expected exit_code and signal
+---set timeout by second
+---@param cmd table<string>
+---@param exit_code integer
+---@param signal integer
+---@param timeout integer
+---@return table<string> | nil, table<string> | nil
+function M.run_system_cmd(cmd, exit_code, signal, timeout)
+  if not cmd or not exit_code or not signal or not timeout then
+    return nil, nil
+  end
+
+  local obj = vim.system(cmd, { text = true, timeout = timeout }):wait()
+  if obj.code ~= exit_code then
+    if signal ~= obj.signal then
+      return nil, nil
+    end
+  end
+  local stdout_table = vim.split(obj.stdout, '\n')
+  local stderr_table = vim.split(obj.stderr, '\n')
+  remove_whiteline(stdout_table)
+  remove_whiteline(stderr_table)
+  return stdout_table, stderr_table
+end
+
+return M
---
+++ tests/specs/utils_spec.lua
@@ -0,0 +1,21 @@
+local utils = require("commit-buf.utils")
+describe("run_system_cmd()", function()
+  it("stdio outpu should be achieved", function()
+    local cmd = { "echo", "hello" }
+    local stdout_table, _ = utils.run_system_cmd(cmd, 0, 0, 100)
+    local expected_table = { "hello" }
+    for i = 1, #expected_table do
+      assert.are.equal(stdout_table[i], expected_table[i])
+    end
+  end)
+  it("stderr output should be achieved", function()
+    local cmd = { "ls", "nonexistent_directory" }
+    local _, stderr_table = utils.run_system_cmd(cmd, 0, 0, 100)
+    local expected_table = {
+      "ls: cannot access 'nonexistent_directory': No such file or directory"
+    }
+    for i = 1, #expected_table do
+      assert.are.equal(stderr_table[i], expected_table[i])
+    end
+  end)
+end)
